2c4fc6ca4f55a7890fde3ddcef3bdb20dcb9f831
pubudu thameera <thameerapubudu@gmail.com>
2023-05-02 20:32:53 +0530

    This is our University Group Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/shared/Layout";
+import Dashboard from "./components/Dashbord";
+import Appointment from "./components/Appoiment";
+import AppointmentDetails from "./components/AppoimentDetails";
+import AddDoctors from "./components/AddDoctors";
+import DoctorDetails from "./components/DoctorDetails";
+import Login from "./components/Login";
+import {AuthProvider} from '../src/security/AuthProvider'
+import Payment from "./components/Payment";
+import Invoice from "./components/invoice/Invoice";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AddReport from "./components/AddReport";
+import Labreport from "./components/Labreport";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ToastContainer />
+    <Router>
+      <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/layout" element={<Layout />}>
+          <Route path="dashbord" element={<Dashboard />} />
+          <Route path="appoiment" element={<Appointment />} />
+          <Route path="appoimentdetails" element={<AppointmentDetails />} />
+          <Route path="adddoctors" element={<AddDoctors />} />
+          <Route path="doctordetails" element={<DoctorDetails /> }/>
+          <Route path="addlab" element={<AddReport /> }/>
+          <Route path="labreporttable" element={<Labreport /> }/>
+          <Route path="payment" element={<Payment />} />
+          <Route path="invoice" element={<Invoice />} />
+       </Route>
+      </Routes>
+      </AuthProvider>
+    </Router>
+   </>
   );
 }
 
